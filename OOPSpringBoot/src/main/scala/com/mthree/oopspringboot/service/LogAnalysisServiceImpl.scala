d532095515903de36ab0e285e827ff8117183a43
sasha-sokol <sasha.sokolniki@gmail.com>
2025-11-17 14:59:13 +0000

    temp

---
+++ OOPSpringBoot/src/main/scala/com/mthree/oopspringboot/service/LogAnalysisServiceImpl.scala
@@ -0,0 +1,32 @@
+package com.mthree.oopspringboot.service
+
+
+import com.mthree.oopspringboot.service.LogAnalysisService
+import com.mthree.api.IGeminiApi
+import com.mthree.oopspringboot.entity.Log
+
+class LogAnalysisServiceImpl extends LogAnalysisService {
+
+    val api: IGeminiApi
+
+    def analyzeLog(log: Log) = {
+        
+    }
+
+    def summarizeLog(log: Log) = {
+
+    }
+
+    def detectAnomalies(log: Log) = {
+
+    }
+
+    def recommendFixes(log: Log) = {
+
+    }
+
+    def devopsChat(prompt: String) = {
+
+    }
+    
+}

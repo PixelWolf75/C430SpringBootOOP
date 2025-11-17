7dc73e61f96970036254d07a4d6ca7789bac197b
Yu Li <104264689+CaitlinLi123@users.noreply.github.com>
2025-11-17 14:18:10 +0000

    feat: add implentations to api implementation

--- OOPSpringBoot/src/main/java/com/mthree/api/ImplGeminiApi.java
+++ OOPSpringBoot/src/main/java/com/mthree/api/ImplGeminiApi.java
@@ -1,28 +1,114 @@
 package com.mthree.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentConfig;
+import com.google.genai.types.GenerateContentResponse;
+import com.google.genai.types.Schema;
+import org.springframework.beans.factory.annotation.Value;
+
 public class ImplGeminiApi implements IGeminiApi{
+
+    @Value("${GEMINI_API_KEY}")
+    private final String apiKey;
+
+    private final Client client;
+
+    public ImplGeminiApi(String apiKey){
+        this.apiKey = apiKey;
+        client = Client.builder().apiKey(apiKey).build();
+    }
+
+    public GenerateContentConfig getConfig(String schemaJsonString){
+        return GenerateContentConfig.builder()
+                        .responseMimeType("application/json")
+                        .candidateCount(1)
+                        .responseSchema(Schema.fromJson(schemaJsonString))
+                        .build();
+    }
+
     @Override
-    public String analyzeLogs(String text) {
-        return "";
+    public String analyzeLogs(String exception) throws JsonProcessingException {
+
+        // format the output like:
+        // Root cause: NullPointerException caused due to missing user profile.
+        // Fix: Add null checks in UserService.getUser().
+        // Impact: Affects login workflows.
+
+        // transform the immutablemap to json string
+        ImmutableMap<String, Object> schema = ImmutableMap.of(
+                "type", "object",
+                "properties", ImmutableMap.of(
+                        "root_cause", ImmutableMap.of("type", "string"),
+                        "fix", ImmutableMap.of("type", "string"),
+                        "impact", ImmutableMap.of("type", "string")
+                )
+                ,
+                "required", ImmutableList.of("root_cause", "fix","impact")
+        );
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(schema);
+
+        // Config
+        GenerateContentConfig config = getConfig(json);
+
+        // Response
+        GenerateContentResponse response =
+                client.models.generateContent("gemini-2.5-flash", exception, config);
+
+        return response.text();
     }
 
     @Override
     public String summarizeIncident(String text) {
-        return "";
+        GenerateContentResponse response =
+                client.models.generateContent("gemini-2.5-flash", "Can you generate a concise 5–10 line summary based on the logs: "+text, null);
+        return response.text();
     }
 
     @Override
-    public String detectAnomaly(String text) {
-        return "";
+    public String detectAnomaly(String text) throws JsonProcessingException {
+        GenerateContentResponse response =
+                client.models.generateContent("gemini-2.5-flash", "Here's the exception: "+text+"\nCan you identify unusual patterns like :sudden spike in errors\n" +
+                        "●\n" +
+                        "memory leaks\n" +
+                        "●\n" +
+                        "slow DB queries\n" +
+                        "●\n" +
+                        "repeating warnings?", null);
+        return response.text();
     }
 
     @Override
     public String generateRecommendFix(String text) {
-        return "";
+        GenerateContentResponse response =
+                client.models.generateContent("gemini-2.5-flash", "Here's the exception: "+text+"\nCan you recommend fixes?", null);
+        return response.text();
     }
 
     @Override
-    public String getResponseFromDevopsChat(String text) {
-        return "";
+    public String getResponseFromDevopsChat(String text) throws JsonProcessingException {
+        ImmutableMap<String, Object> schema = ImmutableMap.of(
+                "type", "object",
+                "properties", ImmutableMap.of(
+                        "Likely cause: ", ImmutableMap.of("type", "string"),
+                        "Fix: ", ImmutableMap.of("type", "string")
+                ),
+                "required", ImmutableList.of("root_cause", "fix","impact")
+        );
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(schema);
+
+        // Config
+        GenerateContentConfig config = getConfig(json);
+
+        // Response
+        GenerateContentResponse response =
+                client.models.generateContent("gemini-2.5-flash", text, config);
+
+        return response.text();
     }
 }

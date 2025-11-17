84aa80d9804031b83eeb4c7212bf0443b270d575
Yu Li <104264689+CaitlinLi123@users.noreply.github.com>
2025-11-17 13:10:11 +0000

    feat: create yu-API-Implementation branch. Add a file to implement api interface

---
+++ OOPSpringBoot/src/main/java/com/mthree/api/ImplGeminiApi.java
@@ -0,0 +1,28 @@
+package com.mthree.api;
+
+public class ImplGeminiApi implements IGeminiApi{
+    @Override
+    public String analyzeLogs(String text) {
+        return "";
+    }
+
+    @Override
+    public String summarizeIncident(String text) {
+        return "";
+    }
+
+    @Override
+    public String detectAnomaly(String text) {
+        return "";
+    }
+
+    @Override
+    public String generateRecommendFix(String text) {
+        return "";
+    }
+
+    @Override
+    public String getResponseFromDevopsChat(String text) {
+        return "";
+    }
+}

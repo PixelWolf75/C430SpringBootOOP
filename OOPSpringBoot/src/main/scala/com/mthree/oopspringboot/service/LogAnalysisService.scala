bf4f303df307442d682fb8031115bf7c8af68aec
sasha-sokol <sasha.sokolniki@gmail.com>
2025-11-17 13:24:34 +0000

    Add return types to AI service trait

--- OOPSpringBoot/src/main/scala/com/mthree/oopspringboot/service/LogAnalysisService.scala
+++ OOPSpringBoot/src/main/scala/com/mthree/oopspringboot/service/LogAnalysisService.scala
@@ -1,18 +1,19 @@
 package com.mthree.oopspringboot.service
 
+import com.mthree.oopspringboot.entity.AIResponse
 import com.mthree.oopspringboot.entity.Log
 
 trait LogAnalysisService {
 
-    def analyzeLog(log: Log);
+    def analyzeLog(log: Log): AIResponse;
 
-    def summarizeLog(log: Log);
+    def summarizeLog(log: Log): AIResponse;
 
-    def detectAnomalies(log: Log);
+    def detectAnomalies(log: Log): AIResponse;
 
-    def recommendFixes(log: Log);
+    def recommendFixes(log: Log): AIResponse;
 
-    def devopsChat(prompt: String);
+    def devopsChat(prompt: String): AIResponse;
 
 }
 

1bab03a882b89443ac691781206c7b2fc09b26d9
Eric Leon <ericleon727@gmail.com>
2025-11-18 11:22:12 +0000

    Temporarely disabled h2 as we aren't using it

--- OOPSpringBoot/src/main/java/com/mthree/Main.java
+++ OOPSpringBoot/src/main/java/com/mthree/Main.java
@@ -5,8 +5,13 @@ package com.mthree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
